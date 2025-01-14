eab635c6bad904d8a819697a572188b2105ed39c
Jakub Milasz <jakubmilasz@gmail.com>
2025-01-14 18:52:04 +0100

    Changing form

--- 09_laravel_tdd/project/resources/views/form.blade.php
+++ 09_laravel_tdd/project/resources/views/form.blade.php
@@ -43,20 +43,6 @@
                 <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Wydarzenie">
             </div>
         </div>
-        <div class="flex flex-wrap -mx-3 mb-2">
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                    City
-                </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque">
-            </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                    Zip
-                </label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210">
-            </div>
-        </div>
         <div class="w-full md:w-full px-3 mb-6 md:mb-0">
             <x-primary-button class="ms-3">
                 {{ __('Submit') }}
@@ -64,7 +50,6 @@
         </div>
     </form>
 </div>
-
 </body>
 </html>
 
