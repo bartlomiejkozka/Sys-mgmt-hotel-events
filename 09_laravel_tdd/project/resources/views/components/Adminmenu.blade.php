5c96526670893860b9bfc9a50c6ed3adaa43e6be
Tomasz Drozd <tdrozd@student.agh.edu.pl>
2025-01-21 17:58:51 +0100

    Welcome page

---
+++ 09_laravel_tdd/project/resources/views/components/Adminmenu.blade.php
@@ -0,0 +1,21 @@
+<nav class="bg-gray-800">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="relative flex h-16 items-center justify-between">
+            <div class="flex shrink-0 items-center">
+                <img class="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
+            </div>
+            <div class="hidden sm:block">
+                <div class="flex space-x-4 justify-center">
+                    <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Strona główna</a>
+                    <a href="/events" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Lista wydarzeń</a>
+                    <a href="/notifications" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Powiadomienia</a>
+                    <a href="/opinions" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Opinie i oceny</a>
+                </div>
+            </div>
+            <div class="flex shrink-0">
+                <a href="/welcome" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Wyloguj się</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
